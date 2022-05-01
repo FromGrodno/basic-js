4b810e9151da17d6b8efa074954b6c21c6036ce9
Hancharuk Aliaksei <hancharuk19257@gmail.com>
2022-05-01 16:48:33 +0300

    task

--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,9 +12,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits( n ) {
+  count = 0;
+  n = String(n)
+  for (let i = 0; i < n.length; i++) {
+     count += Number(n[i])
+  }
+  if (String(count).length > 1) {
+     return getSumOfDigits(count)
+  }
+  else {
+     return count
+  }
 }
 
 module.exports = {
--- src/what-season.js
+++ src/what-season.js
@@ -15,24 +15,33 @@ function getSeason(date) {
   if (arguments.length === 0) {
     return 'Unable to determine the time of year!'
   }
-  else if (Object.prototype.toString.call(date) === "[object Date]" && !isNaN(date)) {
-    if (date.getMonth() >= 0 && date.getMonth() <= 1 || date.getMonth() === 11) {
-      return "winter"
-    }
-    if (date.getMonth() >= 2 && date.getMonth() <= 4) {
-      return "spring"
-    }
-    if (date.getMonth() >= 5 && date.getMonth() <= 7) {
-      return "summer"
-    }
-    if (date.getMonth() >= 8 && date.getMonth() <= 10) {
-      return "autumn"
-    }
+
+  if (Object.prototype.toString.call(date.getMonth) !== '[object Function]') {
+    throw new Error('Invalid date!');
   }
-  else {
-    throw new Error('Invalid date!')
+
+  try {
+    isNaN(date);
+  } catch (err) {
+    throw new Error('Invalid date!');
+  }
+
+  if (date.getMonth() >= 0 && date.getMonth() <= 1 || date.getMonth() === 11) {
+    return "winter"
+  }
+  if (date.getMonth() >= 2 && date.getMonth() <= 4) {
+    return "spring"
+  }
+  if (date.getMonth() >= 5 && date.getMonth() <= 7) {
+    return "summer"
+  }
+  if (date.getMonth() >= 8 && date.getMonth() <= 10) {
+    return "autumn"
   }
 }
+
+
+
 
 module.exports = {
   getSeason
